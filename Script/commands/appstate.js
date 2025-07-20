8265a957c6ce25fccd03541df37b0701b51cc36c
nirob6353 <nirobh6353@gmail.com>
2025-07-20 20:41:10 +0600

    Update appstate.js

--- Script/commands/appstate.js
+++ Script/commands/appstate.js
@@ -1,30 +1,119 @@
-module.exports.config = {
-  name: "appstate",
-  version: "1.0.0",
-  hasPermssion: 2,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-  description: "refresh appstate.json",
-  commandCategory: "Admin",
-  usages: "appstate",
-  cooldowns: 5,
-  dependencies: {
-  }
-};
-
-module.exports.run = async function ({ api, event, args }) {
-  const fs = require("fs-extra");
-  const permission = ["100082395531611", "100086680386976"];
-	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command", event.threadID, event.messageID);
-  let appstate = api.getAppState();
-  // convert JSON object to a string
-  const data = JSON.stringify(appstate);
-  // write file to disk
-  fs.writeFile(`${__dirname}/../../appstate.json`, data, 'utf8', (err) => {
-    if (err) {
-      return api.sendMessage(`Error writing file: ${err}`, event.threadID);
-    } else {
-      return api.sendMessage(`Refreshed appstate successfully`, event.threadID);
+[
+    {
+        "key": "dbln",
+        "value": "%7B%2261571605238307%22%3A%22kuuJqro5%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.745Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "datr",
+        "value": "huV7aPRprYKyycbIB4fWW9A1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.746Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "sb",
+        "value": "huV7aDajMO73nvGyVvvsYh_Z",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.746Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61571605238307",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.746Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "xs",
+        "value": "5%3AA57FXSxrOcS-4g%3A2%3A1752950204%3A-1%3A-1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.746Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "fr",
+        "value": "0k9R1tnZsxORIgTy3.AWdU52DkuKiIYLCBMivmdB4Dv67Xsm2B-u4U7aEWXe2b4yR9yK4.Boe-WG..AAA.0.0.Boe-XC.AWeiBR2wprpT-B_7HeEOvB_Y3qw",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.746Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "locale",
+        "value": "en_US",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.746Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "pas",
+        "value": "61571605238307%3AbeBkAgWAMe",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.746Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.746Z",
+        "lastAccessed": "2025-07-20T14:36:17.746Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.747Z",
+        "lastAccessed": "2025-07-20T14:36:17.747Z"
+    },
+    {
+        "key": "vpd",
+        "value": "v1%3B789x400x1.8000000715255737",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.747Z",
+        "lastAccessed": "2025-07-20T14:36:17.747Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "100430807%3BT%3A29217036",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.747Z",
+        "lastAccessed": "2025-07-20T14:36:17.747Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A2875%3Btimestamp%3A1753022168",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-07-20T14:36:17.747Z",
+        "lastAccessed": "2025-07-20T14:36:17.747Z"
     }
-  });
-
-}
+]
